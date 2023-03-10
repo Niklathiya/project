0a68c38b55de64b5cdafbee187a37181385c5a63
Niklathiya <lathiya810@gmail.com>
2023-03-10 20:29:42 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 // import Pr1 from './Pr1_circlet';
 import Pr2 from './Pr2_explorer';
+import Home from './pages/Home.js'
+import Contact from './pages/Contact';
+import{
+  Routes, Route
+}from "react-router-dom"
+import Layout from './Layout';
+import Exam1 from './Exam1';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   
@@ -8,6 +16,12 @@ const App = () => {
     <div className="App">
         {/* <Pr1/> */}
         <Pr2/>
+        {/* <Exam1/> */}
+      {/* <Routes>
+        <Route path='/Home' element={<Layout><Home/></Layout>}  />
+        <Route path="/Contact" element={<Contact/>} />
+      </Routes> */}
+        
     </div>
   );
 }
